c46bf3a923b9ced6323fb99b908d5bf2d7e4d963
EC2 Default User <ec2-user@ip-172-31-26-134.us-east-2.compute.internal>
2022-01-10 19:19:04 +0000

    finish add place form

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -4,6 +4,7 @@ import './PlaceItem.css';
 import Card from '../../shared/components/UIElements/Card.js';
 import Button from '../../shared/components/FormElements/Button.js';
 import Modal from '../../shared/components/UIElements/Modal.js';
+import Map from '../../shared/components/UIElements/Map.js';
 
 const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false);
@@ -28,7 +29,7 @@ const PlaceItem = (props) => {
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
         <div className="map-container">
-          <h2>THE MAP</h2>
+          <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
       <li className="place-item">
--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -4,7 +4,11 @@ import './Map.css';
 
 const Map = (props) => {
   return(
-    <div className={`map ${props.className}`} style={props.style}>
+    <div 
+      className={`map ${props.className}`} 
+      style={{color: "black", backgroundColor: "lightblue"}}/*{props.style}*/
+    >
+     <h1>THE MAP!</h1>
     </div>
     )
 }
